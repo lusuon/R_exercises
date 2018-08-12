67d7bc71f8a32db740fcff28f137009f2ae5ae5c
lusuon <542343357@qq.com>
2018-08-12 09:26:07 +0800

    Update

--- Week1Quiz.R
+++ Week1Quiz.R
@@ -1,6 +1,14 @@
 #How many missing values are in the Ozone column of this data frame?
 badOzone<-hw1_data[!complete.cases(hw1_data$Ozone),]$Ozone
 length(badOzone)
+# 考虑对is.na()输出的布尔值向量使用sum（）获取NA值数量
+
+
+# that has a TRUE for every NA and FALSE for every numeric value, we can compute the total number of NAs in our data.
+# The trick is to recognize that underneath the surface, R represents TRUE as the number 1 and FALSE as the number 0.
+# Therefore, if we take the sum of a bunch of TRUEs and FALSEs, we get the total number of TRUEs.
+# sum(my_na)
+# [1] 59
 
 #What is the mean of the Ozone column in this dataset? Exclude missing values (coded as NA) from this calculation.
 goodOzoneData<-hw1_data[complete.cases(hw1_data$Ozone),]
