63bd7c52d31dce9bab88feb680b1117206660a05
lusuon <JacksonPony@outlook.com>
2018-08-15 23:43:46 +0800

    sync

--- Week2Quiz.R
+++ Week2Quiz.R
@@ -1,3 +1,24 @@
-pollutantmean<-function(directory,pollutant,id=1:332){
+pollutantmean<-function(directory,pollutant,id){
+  trans<-function(id){
+    order<-1
+    for (i in id) {
+      while (i!=3){
+        i<-paste0('0',i)
+        print(i)
+      }
+      i<-paste0("\'",i,"\'")
+      id[order]<-i
+      order<- order+1
+    }
+    id
+  }
   
+  trans(id)
 }
+  #file_location<-paste(directory,'/',trans(id),'.csv',sep = '')#如：‘./specdata’作为输入
+  #print(file_location)
+  #pollution_data<-read.csv(file_location)
+  #pollution_data
+  #pollutant_choose<-pollution_data[[pollutant]]
+  #mean(pollutant_chooserse,na.rm = TRUE)
+
